a18feaf7f3578e2abd7f587dc1b6676c8851729e
Faizah Ahsan <faizah1992@gmail.com>
2021-01-04 09:11:09 -0600

    eventlistener added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const hexInput = document.getElementById('hexInput')
 const inputColot = document.getElementById('inputColor')
 
+hexInput.addEventListener('keyup', () => {
+    const hex = hexInput.value
+    if(!isValidHex(hex)) return
+})
+
 const isValidHex = (hex) => {
     if(!hex) return false
 
