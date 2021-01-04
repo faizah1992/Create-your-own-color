a8f10529803ef33aa7de2aa6be20f192773bfc4b
Faizah Ahsan <faizah1992@gmail.com>
2021-01-04 09:18:40 -0600

    new function  added

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ const isValidHex = (hex) => {
     return strippedHex.length === 3 || strippedHex.length === 6
 }
 
+const convertHexToRGB = (hex) {
+    if (!isValidHex(hex)) return null
+
+    let strippedHex = hex.replace('#'. '')
+    if(strippedHex.length === 3){
+        strippedHex = strippedHex[0] + strippedHex[0]
+        + strippedHex[1] + strippedHex[1]
+        + strippedHex[2] + strippedHex[2]
+    }
+
+    
+}
